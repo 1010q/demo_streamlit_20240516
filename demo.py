ef15dbd83d13bd11bc2d4ba3a8132e8a0cb31435
1010q <kd1370141@st.kobedenshi.ac.jp>
2024-05-21 13:57:54 +0900

    First commit

---
+++ demo.py
@@ -0,0 +1,48 @@
+import streamlit as st
+import pandas as pd
+from io import StringIO
+import streamlit as st
+import numpy as np
+import time
+
+st.title(':blue[お勉強サイト]')
+
+st.header('')
+
+st.write("Hello world😇","*Hello world*")
+
+st.text('ITの勉強がしたいなら')
+st.link_button("ここで勉強しよう", "https://www.ap-siken.com/apkakomon.php")
+
+df = pd.DataFrame(
+    np.random.randn(10, 2) / [50, 50] + [34.7, 135.24],
+    columns=['lat', 'lon'])
+
+st.map(df)
+
+
+_LOREM_IPSUM = """
+Lorem ipsum dolor sit amet, **consectetur adipiscing** elit, sed do eiusmod tempor
+incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+"""
+
+def stream_data():
+    for word in _LOREM_IPSUM.split(" "):
+        yield word + " "
+        time.sleep(0.02)
+
+    yield pd.DataFrame(
+        np.random.randn(5, 10),
+        columns=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"],
+    )
+
+    for word in _LOREM_IPSUM.split(" "):
+        yield word + " "
+        time.sleep(0.02)
+
+
+if st.button("Stream data"):
+    st.write_stream(stream_data)
+
+st.image('/Applications/MAMP/htdocs/photo-1548407260-da850faa41e3.jpeg', caption='Sunrise by the mountains')
