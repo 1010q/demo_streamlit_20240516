6c68b7d37fc7b9af33b6a5c8756773b268dfc86f
1010q <kd1370141@st.kobedenshi.ac.jp>
2024-05-23 14:11:12 +0900

    dad

--- demo.py
+++ demo.py
@@ -5,9 +5,36 @@ import streamlit as st
 import numpy as np
 import time
 
-st.title(':blue[お勉強サイト]')
+on = st.toggle("背景をグレーにする")
+css = ""
+if on:
+    st.write("一番好きなお寿司はサーモンかいくらです！")
 
-st.header('')
+    image = './pic/green_00080.jpg'
+
+    css = f'''
+    <style>
+        .stApp {{
+            background-image: url({image});
+            background-size: cover;
+            background-position: center;
+            background-color:rgba(255,255,255,0.4);
+        }}
+        .stApp > header {{
+            background-color: transparent;
+        }}
+    </style>
+    '''
+    st.markdown(css, unsafe_allow_html=True)
+    st.balloons()
+else:
+    st.snow()
+
+m = "お勉強サイト"
+st.title(f':blue[{m}]')
+
+color = st.color_picker("好きな色を選んでね", "#ffffff")
+st.write("この色のカラーコードは", color)
 
 st.write("Hello world😇","*Hello world*")
 
@@ -45,5 +72,22 @@ def stream_data():
 if st.button("Stream data"):
     st.write_stream(stream_data)
 
-st.image('./photo-1548407260-da850faa41e3.jpeg', caption='Sunrise by the mountains')
+st.image('./pic/photo-1548407260-da850faa41e3.jpeg', caption='Sunrise by the mountains')
 st.write("キレイな風景だね＾＾")
+
+
+option = st.selectbox(
+    "好きなMrs. GREEN APPLEの曲は？",
+    ("none","ケセラセラ", "Soranji", "ナハトムジーク","僕のこと"))
+if option == "none":
+    st.write("曲を選んで！")
+else:
+    st.write(option,"の歌詞を教えてあげよう")
+if option == "ケセラセラ":
+    st.link_button("著作権的に直接載せれないからリンク踏んでね！", "https://www.uta-net.com/song/336333/")
+elif option == "Soranji":
+    st.link_button("著作権的に直接載せれないからリンク踏んでね！", "https://www.uta-net.com/song/326460/")
+elif option == "ナハトムジーク":
+    st.link_button("著作権的に直接載せれないからリンク踏んでね！", "https://www.uta-net.com/song/348804/")
+elif option == "僕のこと":
+    st.link_button("著作権的に直接載せれないからリンク踏んでね！", "https://www.uta-net.com/song/260738/")
